879a9df056363ce0c4fa1a7f7e6b2b2864c0122b
Taran Dwivedula <taran184@gmail.com>
2020-11-18 09:03:17 -0800

    Additions to script

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,38 @@ myImage.onclick = function() {
         myImage.setAttribute('src', 'images/Screenshot from 2020-11-14 14-24-17.png');
     }
 }
+
+let button = document.querySelector('button');
+let heading = document.querySelector('h1');
+
+function setUserName() {
+    let name = prompt("Please enter your social security number. Just joking please do not sue me. Enter your name.");
+    if(name) {
+        localStorage.setItem('name', name);
+    } else {
+        localStorage.clear();
+    }
+
+}
+
+if(localStorage.getItem('name')) {
+    let storedName = localStorage.getItem('name');
+    heading.textContent = "Epic Affordable Mac Purchased By " + storedName;
+}
+
+button.onclick = function() {
+    setUserName();
+    location.reload();
+    return false;
+}
+
+document.getElementById('button2').onclick = function() {
+    if(!localStorage.getItem('storedName') && !localStorage.getItem('name')) {
+        alert("Name is already cleared!");
+    } else {
+        localStorage.clear();
+        location.reload();
+        return false;
+    }
+    
+}
